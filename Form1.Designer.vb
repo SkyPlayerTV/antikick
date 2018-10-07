af08ca35b238d0415bb1d0557e92576ca4643d36
SkyPlayerTV <mail@oliver-haeusler.de>
2018-10-07 15:52:35 +0200

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 329)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(1358, 24)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300000
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(12, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(1358, 27)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(513, 194)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(73, 21)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Convoi"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(521, 236)
+        Me.MaskedTextBox1.Mask = "90:00"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(46, 22)
+        Me.MaskedTextBox1.TabIndex = 4
+        Me.MaskedTextBox1.ValidatingType = GetType(Date)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(592, 195)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "um"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(573, 239)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Uhr"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(494, 268)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(110, 22)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "von"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(494, 296)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(110, 22)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "nach"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(997, 169)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(12, 33)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(1358, 27)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Interval: 1 min"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.AntiKick.My.Resources.Resources.dsl_banner
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(1382, 453)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(1400, 500)
+        Me.MinimumSize = New System.Drawing.Size(1400, 500)
+        Me.Name = "Form1"
+        Me.Text = "Anti Kick by DSL TRANS"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+    Dim msg1 As String
+    Dim msg2 As String
+    Dim convoi_status As Boolean
+    Dim convoi_fehler As Boolean
+    Dim time As Integer
+    Dim i As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        convoi()
+        If convoi_fehler = True Then Exit Sub 'Bei Fehler --> abbruch
+        MsgBox("Bitte in den ETS2 wechseln und warten bis sich der Chat öffnet. Du kannst ihn dann einfach schließen.")
+        System.Threading.Thread.Sleep(10000)
+        Timer1.Start()
+        SendKeys.Send("y" + "/fix Antikick wurde aktiviert! Du kannst den Chat schließen.")
+        Button1.Enabled = False
+        Button2.Enabled = True
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        convoi()
+        System.Threading.Thread.Sleep(750)
+        SendKeys.SendWait("y" + "Spedition gesucht? ---->  TS: dsltrans.pancraft.de" + "{ENTER}")
+        If convoi_status = True Then
+            System.Threading.Thread.Sleep(750)
+            SendKeys.SendWait("y" + msg1 + "{Enter}")
+            System.Threading.Thread.Sleep(750)
+            SendKeys.SendWait("y" + msg2 + "{Enter}")
+        End If
+        If Timer1.Interval <> time Then Timer1.Interval = time
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+        Button1.Enabled = True
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub convoi()
+        If CheckBox1.Checked = True Then
+            If TextBox1.Text.Contains("von") Or TextBox2.Text.Contains("nach") Then
+                MsgBox("Fehler! Du hast den Start oder das Ende des Convois nicht geändert! bzw. einen Flüchtigkeitsfehler gemacht! Bei Rechtschreibfehlern wird nicht gewarnt!", MsgBoxStyle.Exclamation)
+                convoi_fehler = True
+                convoi_status = False
+                Return
+            End If
+            msg1 = "Heute um " + MaskedTextBox1.Text + " Uhr Convoi von"
+            msg2 = TextBox1.Text + " nach " + TextBox2.Text + ". Es besteht Anwesenheitspflicht auf dem TS dsltrans.pancraft.de."
+            convoi_status = True
+            convoi_fehler = False
+        Else
+            convoi_status = False
+            convoi_fehler = False
+        End If
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        time = 1
+        i = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If i = 0 Then
+            time = 1
+            i = 1
+            Button4.Text = "Intervall: " + time + " min"
+        ElseIf i = 1 Then
+            time = 2
+            i = 2
+            Button4.Text = "Intervall: " + time + " min"
+        ElseIf i = 2 Then
+            time = 3
+            i = 0
+            Button4.Text = "Intervall: " + time + " min"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        convoi()
+        System.Threading.Thread.Sleep(750)
+        SendKeys.SendWait("y" + "Spedition gesucht? ---->  TS: dsltrans.pancraft.de" + "{ENTER}")
+        If convoi_status = True Then
+            System.Threading.Thread.Sleep(750)
+            SendKeys.SendWait("y" + msg1 + "{Enter}")
+            System.Threading.Thread.Sleep(750)
+            SendKeys.SendWait("y" + msg2 + "{Enter}")
+        End If
+        If Timer1.Interval <> time Then Timer1.Interval = time
+    End Sub
+End Class
